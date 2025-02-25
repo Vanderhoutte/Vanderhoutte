16c206d6224f85c84f01a9bbca9ecec8ebbea7c4
Vanderhoutte <WZH790615@163.com>
2025-02-25 06:53:58 +0000

    Rectangle初步

---
+++ Rectangle.hpp
@@ -0,0 +1,16 @@
+#ifndef Rectangle.hpp
+#define Rectangle.hpp
+
+#include<iostream>
+
+class RecTangle {
+    private:
+        double width,height;
+    public:
+        RecTangle(double width,double heiht);//构造
+        RecTangle(const RecTangle &other);//拷贝构造
+        
+
+};
+
+#endif
